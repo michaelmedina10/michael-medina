067d4c3f43fe2b691cc0673ec18922aa04c2184f
Michael Medina <michael.cunha97@hotmail.com>
2023-01-01 09:28:59 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Text color="tomato" fontSize="6xl" fontWeight="bold">
+        Portfolio
+      </Text>
+    </Flex>
+  );
+}
