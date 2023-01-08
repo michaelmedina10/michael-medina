3fadb1f60c607092d8294c590707f54a2938ee83
Michael Medina <michael.cunha97@hotmail.com>
2023-01-08 10:13:57 -0300

    chore: adicionando objetos para retornar titulo e texto que serao renderizados na pagina home

---
+++ src/config/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Kalam', sans-serif`,
+    body: `'Open Sans', sans-serif`,
+  },
+});
+
+export default theme;
---
+++ src/texts/indexText.ts
@@ -0,0 +1,6 @@
+export const titulo = "Hello, I'm Michael Medina";
+export const corpo = ` 
+  I am graduated in computer science and i've worked as data scientist and web developer, using some of
+  the most popular progamming languages as Python, R and Node.
+  Do you want to know more about me?? Click on the link below.
+   `;
